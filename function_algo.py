38696bd81aa1469d0863701ff93022b4e28b0362
Michael.G <michael.mg.09a@gmail.com>
2022-02-09 22:09:18 +0100

    update files

--- function_algo.py
+++ function_algo.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding:utf-8
+import operator
 
+from operator import attrgetter
 import models
 import json
 
@@ -12,12 +14,11 @@ with open("actions.json") as f:
         actions.append(models.Action(i["number_action"], i["price_action"], i["profit_action"]))
 
 for action in actions:
-
-    print("----------------")
     nb_action_for_500_eur = 500 * 1 / action.price_action
-    print(f"{nb_action_for_500_eur:.2f}")
     total = nb_action_for_500_eur * action.price_action
-    print(total)
     action.profit_to_years = total + (total * action.profit_action / 100)
-    # price_init[3] = total + (total * action[N_action].profit_action / 100)
-    print(action.profit_to_years)
+
+    actions_tried_by_best_profit = sorted(actions, key=attrgetter("profit_to_years"), reverse=True)
+
+for action in actions_tried_by_best_profit:
+    print(f"Action N°{action.number_action} - Profit to 2 years : {action.profit_to_years:.2f}")
